82e36014189a99e1f21dfb5ee28f4dcfd286bd41
Mohamed Sharaf <mahmad.sharaf@gmail.com>
2018-10-21 07:28:31 +0200

    Add Deferred Execution Show the difference between Deferred Execution and Direct Execution Show the different categories of Deferred execution like streamed operators and non-streamed operators

--- Queries/Program.cs
+++ Queries/Program.cs
@@ -19,16 +19,16 @@ namespace Queries
 
             };
 
-            var query1 = from movie in movies
-                         where movie.Year > 2000
-                         select movie;
-            var query2 = movies.Filter(m => m.Year > 2000);
-
-            foreach (var movie in query1)
-            {
-                Console.WriteLine(movie.Title);
-            }
-            foreach (var movie in query2)
+            // Where operator just like Filter method in MyLINQ; it depends on lazy operation(deferred execution) which works only we it has to work 
+            // while Count operator is not deferred execution, otherwise it forces the code to execute fully.
+            // So in this scenario, it will loops twice, one fully and one lazily. So to solve this, ToList() operator has to be used as 
+            // will save the result in a list which multiple operators can be done the query without looping more than once. But this will
+            // lose the deferred execution advantages. 
+            // One more disadvantage of deferred execution is throwing exceptions. It skips throwing the exception and it skips the current process. 
+            //x var query = movies.Where(m => m.Year > 2000);
+            var query = movies.Where(m => m.Year > 2000).ToList();
+            Console.WriteLine(query.Count());            
+            foreach (var movie in query)
             {
                 Console.WriteLine(movie.Title);
             }
@@ -37,3 +37,11 @@ namespace Queries
         }
     }
 }
+
+//? Deferred Execution:
+//! It is called lazy execution because it executes only when its execution is needed and starts from the last place has been executed.
+//! Advantages: Saves time and memory for unneeded data executions.
+//! Disadvantages: Does not throw exceptions, the loop can not be used by other operators as the loop execution is unpredicted.
+//! It can be categorized into two categories:
+//! Streaming operators "Where": It only need to read through the source data up until the point the operator produces a result.
+//! Non-streaming operators "OrderBy" : It has to run through all the data to figure out what should be the result.
